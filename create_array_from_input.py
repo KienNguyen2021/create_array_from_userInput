586bc3c3ec7de392af86eb12eeae4a035472d5ed
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-04-24 12:51:39 -0600

    first commit

---
+++ create_array_from_input.py
@@ -0,0 +1,19 @@
+
+# create an array with integer from user input
+lst1 = []
+
+n = int (input("Enter number of elements : "))
+for i in range (0,n):
+  ele = int (input())
+
+  lst.append(ele)
+
+  print (lst)
+
+ # create an array with String from an array
+ 
+array_string = input (" give me their names seperated by commas : \n")
+list_array = array_string.split(",")
+
+print (list_array)
+  
